d3ef8b0e49c8c6f96f17e2a00d0fbd0ab315b1c5
Raven <revanfauzi81@gmail.com>
2022-09-16 10:13:25 +0700

    Change Tailwind to Bulma

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">
+<script>
+    let navbar = [
+        {name: "Home", path: "/"},
+        {name: "My Project", path: "/project"},
+        {name: "About", path: "/about"}
+    ];
+</script>
+
+<nav class="flex items-center">
+    {#each navbar as menu}
+        <li class="mr-6"><a href={menu.path} class="text-blue-500 hover:text-blue-800">{menu.name}</a></li>
+    {/each}
+</nav>
+
+<div class="navbar p-4 flex flex-row w-screen justify-center">
+    <img src="" alt="Kucing Garong" /> 
+    <ul class="flex flex-row justify-evenly">
+        {#each navbar as menu}
+            <li class="flex"><a href={menu.path}>{menu.name}</a></li>
+        {/each}
+    </ul>
+</div>
+
+<h1 class="text-3xl font-bold underline text-center">
     Raven Yrcorvine
 </h1>
---
+++ my-app/src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to About page</h1>
+<a href="/">Home</a>
